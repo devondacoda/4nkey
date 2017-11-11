7367f8f8fe9bb2484462167e1564e97eb0082153
Devon JohnBaptiste <devondevon@outlook.com>
2017-11-11 17:40:13 -0500

    Simplified app logic. Added speaker button. Changed styling

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const dictionary = {};
-
 const definitions = [
   'alardear - to brag',
   'apestar - to stink',
@@ -12,7 +11,7 @@ const definitions = [
   'cuero - leather',
   'desafinar - to sing out of tune',
   'desvariar - to talk nonsense',
-  'enbiablado/a - mischievous',
+  'endiablado/a - mischievous',
   'esmalte de uñas - nail polish',
   'estar llanto - to be crying',
   'fantasma - ghost',
@@ -20,13 +19,13 @@ const definitions = [
   'guiñar - to wink',
   'llave - key',
   'mago/a - wizard',
-  'me da repaso - i am embarrassed',
+  'estoy avergonzado - i am embarrassed',
   'mentir - to lie',
   'palillo - toothpick',
   'plegar - to fold',
   'pomelo - grapefruit',
   'rimar con - to rhyme with',
-  'sonreir(se) - to smile',
+  'sonreír(se) - to smile',
   'tener hipo - to have hiccups',
   'trenzar - to braid',
   'zumbar - to buzz',
@@ -36,8 +35,14 @@ definitions.forEach((entry) => {
   const [entryWord, entryDefinition] = entry.split(' - ');
   dictionary[entryWord] = entryDefinition;
 });
-
+// ────────────────────────────────────────────────────────────────────────────────
+// Word formatting for speech:
 const selectedWord = Object.keys(dictionary)[Math.floor(Math.random() * (definitions.length))];
+// Removes slashes from upcoming pronounciation
+let say = selectedWord.includes('/') ? selectedWord.slice(0, selectedWord.indexOf('/')) :
+  selectedWord;
+// Removes parenthesized word from being pronounced (e.g. 'sonreir(se)' => 'sonreir')
+say = say.includes('(') ? say.slice(0, say.indexOf('(')) : say;
 
 const word = document.querySelector('.word');
 const definition = document.querySelector('.definition');
@@ -45,25 +50,27 @@ word.textContent = selectedWord;
 definition.textContent = dictionary[selectedWord];
 
 // ────────────────────────────────────────────────────────────────────────────────
-// Annyang config for speech recognition
+// Annyang config for speech recognition:
 const { annyang } = window;
-const [microphone, micOn] = document.querySelectorAll('.img-talk, .img-speak');
+const [microphone, micOn] = document.querySelectorAll('.img-mic, .img-mic-on');
 const score = document.querySelector('.score');
 const totalScore = JSON.parse(localStorage.getItem('score'));
 score.textContent = totalScore;
-// localStorage.clear();
-// score.textContent = 0;
-
 
 annyang.setLanguage('es-US');
 annyang.addCallback('result', (res) => {
   let audio = null;
-  if (res.includes(selectedWord)) {
+  const results = res.map(recognizedWord => recognizedWord.toLowerCase());
+  if (results.includes(say)) {
     audio = new Audio('/sounds/correct.wav');
     audio.play();
 
     localStorage.setItem('score', JSON.stringify(totalScore + 2));
-    score.textContent = totalScore;
+    const newTotalScore = JSON.parse(localStorage.getItem('score'));
+    score.textContent = newTotalScore;
+  } else {
+    audio = new Audio('/sounds/try-again.wav');
+    audio.play();
   }
 });
 
@@ -80,15 +87,10 @@ annyang.addCallback('end', () => {
 });
 
 // ────────────────────────────────────────────────────────────────────────────────
-// jQuery event handlers
+// jQuery event handlers:
 const eventHandlers = {
   speak() {
     speechSynthesis.cancel();
-    // Remove slashes from being pronounced
-    let say = selectedWord.includes('/') ? selectedWord.slice(0, selectedWord.indexOf('/')) :
-      selectedWord;
-    // Remove parenthesized word from being pronounced (e.g. 'sonreir(se)' => 'sonreir')
-    say = say.includes('(') ? say.slice(0, say.indexOf('(')) : say;
     const utterance = new SpeechSynthesisUtterance(say);
     utterance.lang = 'es-US';
     speechSynthesis.speak(utterance);
@@ -101,12 +103,14 @@ const eventHandlers = {
 };
 
 // ────────────────────────────────────────────────────────────────────────────────
-// jQuery
+// jQuery:
 const { $ } = window;
 $(document).ready(() => {
   $(document).click((e) => {
+    if (microphone.hasAttribute('hidden')) return;
+
     const firstClassName = e.target.attributes.class.value.split(' ')[0];
-    if (firstClassName === 'img-talk') {
+    if (firstClassName === 'img-mic') {
       eventHandlers.recognizeSpeech();
       return;
     }
@@ -115,14 +119,13 @@ $(document).ready(() => {
       return; // return early so it doesn't scroll
     }
 
-    $(window).scroll();
     if (window.scrollY === 0) {
       $('body, html').animate({ scrollTop: $(window).height() - ($(this).outerHeight(true) / 2) }, 200);
+      window.scrollY = 1;
+    } else {
+      $('body, html').animate({ scrollTop: 0 }, 200);
+      window.scrollY = 0;
     }
-  });
-
-  $(window).scroll(() => {
-    eventHandlers.speak();
   });
 });
 
